8eb03e01cd53930e107a5780d70fe0528b1bb021
Carlos Sanz <carlos.sanzpenas@gmail.com>
2022-10-03 13:10:25 +0200

    main.java

---
+++ main.java
@@ -0,0 +1,17 @@
+public class main {
+
+    public static void main (String[] args){
+        int param1 = 1;        
+        int param2 = 3;
+        int param2 = 5;
+        
+        var valor sumo (param1, param2, param3);
+        
+        System.out.println(valor);
+    }
+
+    public static void suma (int a, int b, int c){
+        return a + b + c;
+    }
+
+}
