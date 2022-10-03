01aeae322b837fcdcdeeb91c2f1e7a4b6bdf42a3
Carlos Sanz <carlos.sanzpenas@gmail.com>
2022-10-03 22:06:01 +0200

    Ejercicio suma

--- main.java
+++ main.java
@@ -1,17 +1,15 @@
+package com.open_bootcamp;
+
+import java.util.ArrayList;
+
 public class main {
-
     public static void main (String[] args){
-        int param1 = 1;        
+        int param1 = 1;
         int param2 = 3;
-        int param2 = 5;
-        
-        var valor sumo (param1, param2, param3);
-        
-        System.out.println(valor);
+        int param3 = 5;
+        System.out.println(sumo (param1, param2, param3));
     }
-
-    public static void suma (int a, int b, int c){
+    public static int sumo (int a, int b, int c){
         return a + b + c;
     }
-
 }
